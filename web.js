e0fff936475c4fbf78aaf558aa285b7eac96d6c0
Calvin <146539021+Calvinmuemah@users.noreply.github.com>
2024-03-26 13:39:05 -0700

    Update web.js

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ const images=document.querySelector(".images");
 
 // to do:put your access key from unsplash here
 const accesskey="y30KZ74eKSZ12cNv_NB09pFLmffpTFCWOMDbSzrhwGY";
-const page=2;
-const keyword="birds";
+const page=5;
+const keyword="houses";
 async function getimages(){
     const res =await fetch(
     ` https://api.unsplash.com/search/photos?page=${page}&query=
